ea6d078cb223a3fa29f8db69e0f62f45fc1fe4df
Juliano Metzker <julianometzker87@gmail.com>
2024-01-26 15:50:29 -0300

    items of nav

--- main.py
+++ main.py
@@ -8,9 +8,52 @@ def main(page: Page):
     # Page title
     page.title = "MetzkerTech Portfolio"
     
-
+    # hover of nav items
+    def _change_text_color(e):
+        if e.control.content.color == 'black':
+            e.control.content.color = 'white70'
+            e.control.content.update()
+        else:
+            e.control.content.color = 'black'
+            e.control.content.update()
     # navbar
-    _nav = Row()
+    _nav = Row(
+        alignment='end',
+        controls=[
+            Container(
+                padding=padding.only(right=25),
+                height=60,
+                content=Row(
+                    controls=[
+                        Container(
+                            on_hover=lambda e: _change_text_color(e),
+                            content=Text(
+                                'About Us', 
+                                weight="w600", 
+                                color="black",
+                            ),
+                        ),
+                        Container(
+                            on_hover=lambda e: _change_text_color(e),
+                            content=Text(
+                                'Contact',
+                                weight="w600",
+                                color="black"
+                            ),
+                        ),
+                        Container(
+                            on_hover=lambda e: _change_text_color(e),
+                            content=Text(
+                                'Services',
+                                weight="w600",
+                                color="black"
+                            ),
+                        ),
+                    ],
+                ),
+            ),
+        ],
+    )
 
     # main column
     _main_col = Column()
