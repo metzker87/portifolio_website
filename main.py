c93944fda42df24203060bb9a59408097f9e74b0
Juliano Metzker <julianometzker87@gmail.com>
2024-01-26 16:40:33 -0300

    initial title configuration

--- main.py
+++ main.py
@@ -8,6 +8,14 @@ def main(page: Page):
     # Page title
     page.title = "MetzkerTech Portfolio"
     
+    def on_resize(e):
+        if page.width <= 730:
+            _nav.controls[0].visible = False
+            _nav.update()
+        else:
+            _nav.controls[0].visible = True
+            _nav.update()
+
     # hover of nav items
     def _change_text_color(e):
         if e.control.content.color == 'black':
@@ -55,9 +63,21 @@ def main(page: Page):
         ],
     )
 
+    # title
+    _title = ResponsiveRow(
+        alignment = "center",
+        controls=[
+            Container(
+                alignment=alignment.top_center,
+                content=Text('MetzkerTech Portfolio'),
+            ),
+        ],
+    )
+
     # main column
     _main_col = Column()
     _main_col.controls.append(_nav)
+    _main_col.controls.append(_title)
 
 
     # bg container
@@ -74,7 +94,7 @@ def main(page: Page):
     )
     
     page.add(_background)
-
+    page.on_resize = on_resize
 
 if __name__ == "__main__":
     flet.app(target=main)
